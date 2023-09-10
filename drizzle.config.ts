31c8978259cbba1d3a6006fcb2bf687633d86410
Jp Sia <johnpaulsia4@gmail.com>
2023-09-11 02:55:55 +0800

    feat: add drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv'
+import type { Config } from 'drizzle-kit'
+
+dotenv.config({ path: '.env.local' })
+
+export default {
+	schema: './src/lib/db/schema.ts',
+	out: './drizzle',
+	driver: 'pg',
+	dbCredentials: {
+		connectionString: process.env.DATABASE_URL!,
+	},
+} satisfies Config
