920f49ca2005f320ac64322b2da74061acb5637e
Jp Sia <johnpaulsia4@gmail.com>
2023-09-12 23:33:06 +0800

    chore: move cn function to utils folder

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from 'next-themes'
 
-import { cn } from '@/lib/utils'
+import { cn } from '@/lib/utils/cn'
 import { Button } from '@/components/ui/button'
 import { Icons } from '@/components/icons'
 
