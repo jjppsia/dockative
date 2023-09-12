dc3f2614b2ead113aa3dcacd45533c83e7fe13b9
Jp Sia <johnpaulsia4@gmail.com>
2023-09-12 23:31:26 +0800

    feat: add dependencies for pinecone, econding, pdf-parse, and langchain

--- src/env.mjs
+++ src/env.mjs
@@ -7,6 +7,8 @@ export const env = createEnv({
 		CLERK_SECRET_KEY: z.string().min(1),
 		UPLOADTHING_SECRET: z.string().min(1),
 		UPLOADTHING_APP_ID: z.string().min(1),
+		PINECONE_ENVIRONMENT: z.string().min(1),
+		PINECONE_API_KEY: z.string().min(1),
 	},
 	client: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
