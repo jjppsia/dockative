90fe55bc25d970801303c30016ba5c2e63094a1b
Jp Sia <johnpaulsia4@gmail.com>
2023-09-13 10:45:27 +0800

    feat: update route for create-chat api

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,20 +1,50 @@
 import { NextResponse } from 'next/server'
+import { auth } from '@clerk/nextjs'
 import { z } from 'zod'
 
+import { db } from '@/lib/db'
+import { chats } from '@/lib/db/schema'
+import { loadPdfIntoPinecone } from '@/lib/pinecone'
+
 const createChatSchema = z.object({
-	file_key: z.string(),
-	file_name: z.string(),
+	fileKey: z.string(),
+	fileName: z.string(),
+	fileUrl: z.string(),
 })
 
 export async function POST(req: Request) {
 	try {
+		const { userId } = auth()
+
+		if (!userId) {
+			return NextResponse.json({
+				status: 401,
+				error: 'Unauthorized',
+			})
+		}
+
 		const json = await req.json()
-		const body = createChatSchema.parse(json)
+		const { fileKey, fileName, fileUrl } = createChatSchema.parse(json)
+
+		await loadPdfIntoPinecone(fileKey, fileName, fileUrl)
+
+		const chatId = await db
+			.insert(chats)
+			.values({
+				userId,
+				fileKey,
+				pdfName: fileName,
+				pdfUrl: fileUrl,
+			})
+			.returning({
+				insertedId: chats.id,
+			})
 
 		return NextResponse.json({
 			status: 200,
-			message: 'Chat created successfully',
-			data: body,
+			data: {
+				chatId: chatId[0].insertedId,
+			},
 		})
 	} catch (error) {
 		if (error instanceof z.ZodError) {
