fe5dab60a0bced0239c90b50394598a8c095fcba
Jp Sia <johnpaulsia4@gmail.com>
2023-09-14 13:13:21 +0800

    feat: add chat component

---
+++ src/components/chat.tsx
@@ -0,0 +1,13 @@
+export default function Chat() {
+	return (
+		<main className='lg:pl-72'>
+			<div className='xl:pr-[calc(100%/2)]'>
+				<div className='px-4 py-10 sm:px-6 lg:px-8 lg:py-6'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam omnis
+					vero, nam qui repellendus ipsam. Dicta, hic rem quod nemo eligendi
+					vitae quam ratione aperiam, laudantium id maxime, repellat architecto.
+				</div>
+			</div>
+		</main>
+	)
+}
