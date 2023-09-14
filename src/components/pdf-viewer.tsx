d6615d65b56c14fae883456a210d93c051c9156d
Jp Sia <johnpaulsia4@gmail.com>
2023-09-14 13:13:37 +0800

    feat: add pdf-viewer component

---
+++ src/components/pdf-viewer.tsx
@@ -0,0 +1,10 @@
+export default function PdfViewer() {
+	return (
+		<aside className='fixed inset-y-0 right-0 hidden w-[calc((100%-18rem)/2)] overflow-y-auto border-l px-4 py-6 sm:px-6 lg:px-8 xl:block'>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum atque eius
+			ducimus. Iste aut, dignissimos sit unde ex minima commodi, molestias quam
+			harum perferendis quos reprehenderit voluptatum voluptatibus explicabo
+			quidem!
+		</aside>
+	)
+}
