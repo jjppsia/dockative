25bb31388c2ba6f514ce87adc1ae9995bcad1918
Jp Sia <johnpaulsia4@gmail.com>
2023-09-14 13:13:27 +0800

    feat: add icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,9 +1,25 @@
-import { ArrowRight, Inbox, Loader2, Moon, Sun } from 'lucide-react'
+import {
+	ArrowRight,
+	Inbox,
+	Loader2,
+	Menu,
+	MessageSquare,
+	Moon,
+	MoreHorizontal,
+	Plus,
+	Sun,
+	X,
+} from 'lucide-react'
 
 export const Icons = {
 	arrowRight: ArrowRight,
 	inbox: Inbox,
 	loader2: Loader2,
+	menu: Menu,
+	messageSquare: MessageSquare,
 	moon: Moon,
+	moreHorizontal: MoreHorizontal,
+	plus: Plus,
 	sun: Sun,
+	x: X,
 }
