60356577c78c676ed83f519113e88758328ca12f
Jp Sia <johnpaulsia4@gmail.com>
2023-09-15 14:50:00 +0800

    feat: remove uploadthing, use s3 bucket for pdf storage and react-dropzone

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -1,102 +1,112 @@
 'use client'
 
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { useMutation } from '@tanstack/react-query'
-import { UploadDropzone } from '@uploadthing/react'
 import axios from 'axios'
+import { useDropzone } from 'react-dropzone'
 import toast from 'react-hot-toast'
 
-import { cn } from '@/lib/utils/cn'
-import { buttonVariants } from '@/components/ui/button'
+import { uploadToS3 } from '@/lib/s3/client'
 import { Icons } from '@/components/icons'
-import { OurFileRouter } from '@/app/api/uploadthing/core'
+import { CreateChatSchema } from '@/app/api/create-chat/route'
 
 export default function FileUpload() {
+	const [isUploading, setIsUploading] = useState(false)
 	const router = useRouter()
-	const { mutate } = useMutation({
-		mutationFn: async ({
-			fileKey,
-			fileName,
-			fileUrl,
-		}: {
-			fileKey: string
-			fileName: string
-			fileUrl: string
-		}) => {
-			const response = await axios.post('/api/create-chat', {
+
+	const { mutate, isLoading } = useMutation({
+		mutationFn: async ({ fileKey, fileName }: CreateChatSchema) => {
+			const reponse = await axios.post('/api/create-chat', {
 				fileKey,
 				fileName,
-				fileUrl,
 			})
 
-			return response.data
+			return reponse.data
+		},
+	})
+
+	const { getRootProps, getInputProps } = useDropzone({
+		accept: { 'application/pdf': ['.pdf'] },
+		maxFiles: 1,
+		maxSize: 4 * 1024 * 1024,
+		onDropRejected: (fileRejections) => {
+			const { errors } = fileRejections[0]
+
+			if (errors[0].code === '') {
+				toast.error('File size must be less than 4MB.')
+				return
+			}
+
+			if (errors[0].code === 'file-invalid-type') {
+				toast.error('File type must be PDF.')
+				return
+			}
+
+			if (errors[0].code === 'too-many-files') {
+				toast.error('Only one file can be uploaded at a time.')
+				return
+			}
+
+			toast.error('There was an error uploading your file.')
+		},
+		onDropAccepted: async (acceptedFiles) => {
+			const file = acceptedFiles[0]
+
+			try {
+				setIsUploading(true)
+
+				const result = await uploadToS3(file)
+
+				if (!result) {
+					toast.error('There was an error uploading your file.')
+					return
+				}
+
+				mutate(result, {
+					onSuccess: (data) => {
+						if (!data.sucess) {
+							toast.error('There was an error creating your chat.')
+							return
+						}
+
+						toast.success('Chat created successfully!')
+						// router.push(`/chat/${data.chatId}`)
+					},
+					onError: () => toast.error('There was an error creating your chat.'),
+				})
+			} catch (error) {
+				toast.error('There was an error uploading your file.')
+			} finally {
+				setIsUploading(false)
+			}
 		},
 	})
 
 	return (
-		<div className='cursor-pointer rounded-2xl border p-2'>
-			<UploadDropzone<OurFileRouter>
-				content={{
-					label: 'Choose a PDF file or drag it here',
-					allowedContent: 'PDF (4MB)',
-					button: ({ isUploading }) => {
-						return isUploading ? (
-							<Icons.loader2 className='h-4 w-4 animate-spin' />
-						) : null
-					},
-				}}
-				appearance={{
-					container: ({ isUploading }) =>
-						cn(
-							'mt-0 border-border ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
-							isUploading && 'cursor-not-allowed'
-						),
-					uploadIcon: 'text-muted-foreground',
-					label: 'text-foreground pointer-events-none',
-					allowedContent: 'text-muted-foreground',
-					button: ({ isUploading }) =>
-						cn(
-							buttonVariants({ size: 'sm' }),
-							isUploading && 'after:content-none'
-						),
-				}}
-				endpoint='pdfUploader'
-				onClientUploadComplete={(res) => {
-					toast.success('File uploaded successfully!')
-
-					if (!res) {
-						toast.error('Something went wrong.')
-						return
-					}
-
-					const { key: fileKey, name: fileName, url: fileUrl } = res[0]
-
-					mutate(
-						{ fileKey, fileName, fileUrl },
-						{
-							onSuccess: ({ data, status }) => {
-								switch (status) {
-									case 200:
-										toast.success('Chat created successfully!')
-										router.push(`/chat/${data.chatId}`)
-										break
-									case 429:
-										toast.error('Rate limit reached for requests')
-										break
-									default:
-										toast.error('There was an error creating the chat.')
-								}
-							},
-							onError: (error) => {
-								// eslint-disable-next-line no-console
-								console.error(error)
-								toast.error('There was an error creating the chat.')
-							},
-						}
-					)
-				}}
-				onUploadError={(error) => toast.error(error.message)}
-			/>
+		<div className='rounded-[20px] border bg-background p-2'>
+			<div
+				{...getRootProps({
+					className:
+						'flex h-32 cursor-pointer flex-col items-center justify-center rounded-xl border-2 border-dashed text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring',
+				})}
+			>
+				<input {...getInputProps()} />
+				{isUploading || isLoading ? (
+					<>
+						<Icons.loader2 className='h-8 w-8 animate-spin text-primary' />
+						<p className='mt-3'>
+							{isUploading ? 'Uploading your file...' : 'Creating your chat...'}
+						</p>
+					</>
+				) : (
+					<>
+						<Icons.fileBox className='h-8 w-8' />
+						<p className='mt-3'>Choose a file or drag it here.</p>
+						<small className='text-xs'>PDF (4MB max)</small>
+					</>
+				)}
+			</div>
 		</div>
 	)
 }
--- src/env.mjs
+++ src/env.mjs
@@ -8,14 +8,21 @@ export const env = createEnv({
 		OPENAI_API_KEY: z.string().min(1),
 		PINECONE_API_KEY: z.string().min(1),
 		PINECONE_ENVIRONMENT: z.string().min(1),
-		UPLOADTHING_SECRET: z.string().min(1),
-		UPLOADTHING_APP_ID: z.string().min(1),
 	},
 	client: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+		NEXT_PUBLIC_S3_ACCESS_KEY_ID: z.string().min(1),
+		NEXT_PUBLIC_S3_BUCKET_NAME: z.string().min(1),
+		NEXT_PUBLIC_S3_SECRET_ACCESS_KEY: z.string().min(1),
+		NEXT_PUBLIC_S3_REGION: z.string().min(1),
 	},
 	experimental__runtimeEnv: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
 			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+		NEXT_PUBLIC_S3_ACCESS_KEY_ID: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
+		NEXT_PUBLIC_S3_BUCKET_NAME: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+		NEXT_PUBLIC_S3_SECRET_ACCESS_KEY:
+			process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
+		NEXT_PUBLIC_S3_REGION: process.env.NEXT_PUBLIC_S3_REGION,
 	},
 })
