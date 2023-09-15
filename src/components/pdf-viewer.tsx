3fab91f3097ca9806b0464aef17fd8c261562c74
Jp Sia <johnpaulsia4@gmail.com>
2023-09-15 14:51:43 +0800

    feat: use iframe to embed pdf

--- src/components/pdf-viewer.tsx
+++ src/components/pdf-viewer.tsx
@@ -1,10 +1,14 @@
-export default function PdfViewer() {
+type PdfViewerProps = {
+	pdfUrl: string
+}
+
+export default function PdfViewer({ pdfUrl }: PdfViewerProps) {
 	return (
-		<aside className='fixed inset-y-0 right-0 hidden w-[calc((100%-18rem)/2)] overflow-y-auto border-l px-4 py-6 sm:px-6 lg:px-8 xl:block'>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum atque eius
-			ducimus. Iste aut, dignissimos sit unde ex minima commodi, molestias quam
-			harum perferendis quos reprehenderit voluptatum voluptatibus explicabo
-			quidem!
+		<aside className='fixed inset-y-0 right-0 hidden w-[calc((100%-18rem)/2)] border-l px-4 py-6 sm:px-6 lg:px-8 xl:block'>
+			<iframe
+				src={`https://docs.google.com/gview?url=${pdfUrl}&embedded=true`}
+				className='h-full w-full'
+			/>
 		</aside>
 	)
 }
