dfc6406c29618380fceaca0077f1cd44585fdf52
Jp Sia <johnpaulsia4@gmail.com>
2023-09-16 00:32:46 +0800

    chore: add request count logs for debugging purposes

--- src/lib/embeddings.ts
+++ src/lib/embeddings.ts
@@ -1,8 +1,15 @@
+/* eslint-disable no-console */
 import OpenAI from 'openai'
 
 const openai = new OpenAI()
 
+let requestCount = 0
+
 export const getEmbeddings = async (text: string) => {
+	requestCount += 1
+
+	console.log(`Request count: ${requestCount}`)
+
 	try {
 		const embedding = await openai.embeddings.create({
 			input: text.replace(/\n/g, ''),
@@ -11,7 +18,6 @@ export const getEmbeddings = async (text: string) => {
 
 		return embedding.data[0].embedding
 	} catch (error) {
-		// eslint-disable-next-line no-console
 		console.error(`Error from getting embeddings: ${error}`)
 		throw error
 	}
