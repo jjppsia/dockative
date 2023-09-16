9527ace9c45df155f94ea052ab92f23c49109725
Jp Sia <johnpaulsia4@gmail.com>
2023-09-17 07:48:43 +0800

    feat: update chat component

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,11 +1,52 @@
-export default function Chat() {
+'use client'
+
+import { useChat } from 'ai/react'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import ChatMessage from '@/components/chat-message'
+import { Icons } from '@/components/icons'
+
+type ChatProps = {
+	chatId: number
+}
+
+export default function Chat({ chatId }: ChatProps) {
+	const { messages, input, handleInputChange, handleSubmit } = useChat({
+		api: '/api/chat',
+		body: { chatId },
+	})
+
 	return (
 		<main className='lg:pl-72'>
 			<div className='xl:pr-[calc(100%/2)]'>
-				<div className='px-4 py-10 sm:px-6 lg:px-8 lg:py-6'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam omnis
-					vero, nam qui repellendus ipsam. Dicta, hic rem quod nemo eligendi
-					vitae quam ratione aperiam, laudantium id maxime, repellat architecto.
+				<div className='relative flex min-h-screen flex-col px-4 py-10 dark:bg-neutral-800 sm:px-6 lg:px-8 lg:py-6'>
+					<div className='flex-1'>
+						<ChatMessage messages={messages} />
+					</div>
+
+					<footer className='sticky inset-x-0 bottom-6 mx-auto mt-10 w-full max-w-xl lg:mt-6'>
+						<form
+							onSubmit={handleSubmit}
+							className='relative flex items-center'
+						>
+							<Input
+								type='text'
+								value={input}
+								className='shadow-md'
+								placeholder='Send a message'
+								onChange={handleInputChange}
+							/>
+							<Button
+								variant='ghost'
+								size='sm'
+								className='absolute right-4 h-auto p-1'
+							>
+								<Icons.send className='h-4 w-4' aria-hidden='true' />
+								<span className='sr-only'>Send</span>
+							</Button>
+						</form>
+					</footer>
 				</div>
 			</div>
 		</main>
