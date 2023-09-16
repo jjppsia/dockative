1f64f152e01ca96eed51a687eb57eff590f94bfb
Jp Sia <johnpaulsia4@gmail.com>
2023-09-17 07:53:51 +0800

    feat: add an index file for utils

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,4 @@
+export { cn } from './cn'
+export { getPineconeClient } from './get-pinecone-client'
+export { removeNonAscii } from './remove-non-ascii'
+export { truncateStringByBytes } from './truncate-string'
