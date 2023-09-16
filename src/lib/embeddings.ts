036bd1e7a050edb4b72554042cd4ceac485b9975
Jp Sia <johnpaulsia4@gmail.com>
2023-09-17 07:51:31 +0800

    chore: remove unnecessary request count logging

--- src/lib/embeddings.ts
+++ src/lib/embeddings.ts
@@ -1,23 +1,17 @@
-/* eslint-disable no-console */
 import OpenAI from 'openai'
 
 const openai = new OpenAI()
 
-let requestCount = 0
-
 export const getEmbeddings = async (text: string) => {
-	requestCount += 1
-
-	console.log(`Request count: ${requestCount}`)
-
 	try {
 		const embedding = await openai.embeddings.create({
-			input: text.replace(/\n/g, ''),
+			input: text.replace(/\n/g, ' '),
 			model: 'text-embedding-ada-002',
 		})
 
 		return embedding.data[0].embedding
 	} catch (error) {
+		/* eslint-disable no-console */
 		console.error(`Error from getting embeddings: ${error}`)
 		throw error
 	}
