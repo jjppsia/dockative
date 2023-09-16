b5333d9b9b8f302e502fc18f6bc84662976fea3c
Jp Sia <johnpaulsia4@gmail.com>
2023-09-17 07:47:35 +0800

    chore: update response structure

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 
 import { db } from '@/lib/db'
 import { chats } from '@/lib/db/schema'
-import { loadPdfIntoPinecone } from '@/lib/pinecone'
+import { loadS3IntoPinecone } from '@/lib/pinecone'
 import { getS3Url } from '@/lib/s3/client'
 
 const createChatSchema = z.object({
@@ -29,7 +29,7 @@ export async function POST(req: Request) {
 		const json = await req.json()
 		const { fileKey, fileName } = createChatSchema.parse(json)
 
-		const documents = await loadPdfIntoPinecone(fileKey)
+		const documents = await loadS3IntoPinecone(fileKey)
 
 		const chatId = await db
 			.insert(chats)
@@ -43,27 +43,40 @@ export async function POST(req: Request) {
 				insertedId: chats.id,
 			})
 
-		return NextResponse.json({
-			success: true,
-			status: 200,
-			data: {
+		return NextResponse.json(
+			{
+				success: true,
 				chatId: chatId[0].insertedId,
 				documents,
 			},
-		})
+			{
+				status: 201,
+				statusText: 'Created.',
+			}
+		)
 	} catch (error) {
 		if (error instanceof z.ZodError) {
-			return NextResponse.json({
-				success: false,
-				status: 422,
-				error: error.issues,
-			})
+			return NextResponse.json(
+				{
+					success: false,
+					error: error.issues,
+				},
+				{
+					status: 422,
+					statusText: 'Unprocessable Entity.',
+				}
+			)
 		}
 
-		return NextResponse.json({
-			success: false,
-			status: 500,
-			error: 'Internal Server Error.',
-		})
+		return NextResponse.json(
+			{
+				success: false,
+				error,
+			},
+			{
+				status: 500,
+				statusText: 'Internal Server Error.',
+			}
+		)
 	}
 }
