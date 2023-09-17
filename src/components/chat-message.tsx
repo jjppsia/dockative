4eaa2df35b2a73accb8a23bd6ac2a984a9b22033
Jp Sia <johnpaulsia4@gmail.com>
2023-09-18 07:30:33 +0800

    feat: add loading indicator and update styles

--- src/components/chat-message.tsx
+++ src/components/chat-message.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
 import { Message } from 'ai/react'
 
 import { cn } from '@/lib/utils'
+import { Icons } from '@/components/icons'
 
 type ChatMessageProps = {
 	messages: Message[]
+	isLoading: boolean
 }
 
-export default function ChatMessage({ messages }: ChatMessageProps) {
+export default function ChatMessage({ messages, isLoading }: ChatMessageProps) {
+	if (isLoading) {
+		return (
+			<div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+				<Icons.loader2 className='h-6 w-6 animate-spin' />
+				<span className='sr-only'>Loading</span>
+			</div>
+		)
+	}
+
 	return (
 		<div className='flex flex-col space-y-6'>
 			{messages.map((message) => {
@@ -23,8 +33,8 @@ export default function ChatMessage({ messages }: ChatMessageProps) {
 							className={cn(
 								'rounded-lg border p-2 text-sm shadow-md',
 								message.role === 'user'
-									? 'max-w-[48%] bg-primary text-primary-foreground'
-									: 'max-w-[48%] bg-secondary'
+									? 'max-w-[80%] bg-primary text-primary-foreground md:max-w-[60%] xl:max-w-[80%] 2xl:max-w-[48%]'
+									: 'max-w-[80%] bg-secondary md:max-w-[60%] xl:max-w-[90%] 2xl:max-w-[48%]'
 							)}
 						>
 							<p>{message.content}</p>
