61f9648e2857e29adb619da6f3b70a0b8b746666
Jp Sia <johnpaulsia4@gmail.com>
2023-09-19 07:32:28 +0800

    feat: add ai response to be markdown

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -49,7 +49,7 @@ export async function POST(req: Request) {
 		AI is a well-behaved and well-mannered individual.
 		AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
 		AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
-		AI assistant is a big fan of Pinecone and Vercel.
+		AI assistant is a big fan of Pinecone and Vercel. AI assistant response must be formatted as markdown.
 		START CONTEXT BLOCK
 		${context}
 		END OF CONTEXT BLOCK
